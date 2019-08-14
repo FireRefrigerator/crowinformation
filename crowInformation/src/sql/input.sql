a56b45832ef8b2e2ee74997d4360700c55afe50d
FireRefrigerator <33680609+FireRefrigerator@users.noreply.github.com>
2019-08-14 21:45:04 +0800

    Create input.sql

---
+++ crowInformation/src/sql/input.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `news` (
+  `id` varchar(255) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `autor` varchar(255) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `publishdate` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1
