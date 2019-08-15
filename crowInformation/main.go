99a25341e5ca4a0be7af0d7ca6d86b3f3dcb8319
FireRefrigerator <33680609+FireRefrigerator@users.noreply.github.com>
2019-08-15 20:28:36 +0800

    Update main.go

--- crowInformation/main.go
+++ crowInformation/main.go
@@ -2,13 +2,19 @@
 package main
 
 import (
-	_ "crowInformation/routers"
-	"fmt"
+	_ "service/routers"
+	_ "service/model"
+	_ "service/dao"
 
 	"github.com/astaxie/beego"
+	"fmt"
+	"service/dao"
+	"service/model"
 )
 
 func main() {
 	fmt.Println("Hello crow news!")
+	// dao.Connect(new(model.UserInfo))
+	dao.Connect(new(model.News))
 	beego.Run()
 }
